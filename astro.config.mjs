4345da70271f67069e3fb9f37ac2803f117b0852
Shilo954 <shilotech@protonmail.com>
2024-09-01 10:09:44 +0000

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,29 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    image: {
-        domains: ['cdn.sanity.io']
-    },
-    vite: {
-        server: {
-            hmr: { path: '/vite-hmr/' }
-        },
-        optimizeDeps: { exclude: ['fsevents'] }
-    },
-    integrations: [
-        tailwind({
-            applyBaseStyles: false
-        })
-    ],
+  image: {
+    domains: ['cdn.sanity.io']
+  },
+  vite: {
     server: {
-        port: 3000
+      hmr: {
+        path: '/vite-hmr/'
+      }
+    },
+    optimizeDeps: {
+      exclude: ['fsevents']
     }
+  },
+  integrations: [tailwind({
+    applyBaseStyles: false
+  })],
+  server: {
+    port: 3000
+  },
+  output: "server",
+  adapter: vercel()
 });
